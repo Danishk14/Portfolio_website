20c7d0b85b1b9b17b5f1f873cdc14d80586149d9
Danish <danishk2401@gmail.com>
2023-04-04 12:54:23 +0530

    Created a portfolio website using HTML ,CSS and JS

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".multiple-text",{
+    strings:["Data Science Student","Web developer","Machine Learning student","Football Player"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:200,
+    loop:true
+})
